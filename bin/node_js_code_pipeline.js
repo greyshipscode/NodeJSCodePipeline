d35f70215924f3b2cb65dd3ea6ef91c398e27153
Grey Newell <greyshipscode@gmail.com>
2020-11-23 17:14:16 -0500

    + refactor app to respect stack name from env

--- bin/node_js_code_pipeline.js
+++ bin/node_js_code_pipeline.js
@@ -2,6 +2,7 @@
 
 const cdk = require('@aws-cdk/core');
 const { NodeJsCodePipelineStack } = require('../lib/node_js_code_pipeline-stack');
+const stackName = require('../lib/stack_name');
 
 const app = new cdk.App();
-new NodeJsCodePipelineStack(app, 'NodeJsCodePipelineStack');
+new NodeJsCodePipelineStack(app, stackName);

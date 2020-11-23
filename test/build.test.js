4804eee4c221dfb6c6c37ec0b0eb008a8ae48a24
Grey Newell <greyshipscode@gmail.com>
2020-11-23 18:28:33 -0500

    - tests for new components to be added

---
+++ test/build.test.js
@@ -0,0 +1,11 @@
+const { expect, matchTemplate, MatchStyle, haveResource } = require('@aws-cdk/assert');
+const cdk = require('@aws-cdk/core');
+const NodeJsCodePipeline = require('../lib/node_js_code_pipeline-stack');
+
+test('CodeCommit Repo', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new NodeJsCodePipeline.NodeJsCodePipelineStack(app, 'MyTestStack');
+    // THEN
+    expect(stack).to(haveResource('AWS::CodeBuild::Project'));
+});

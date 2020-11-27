884a7547695f3a052401936464b7417c36ae8065
Grey Newell <greyshipscode@gmail.com>
2020-11-26 22:19:31 -0500

    + refactor to spawn instead of exec for stdio pipe

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 #! /usr/bin/env node
 'use strict';
-const { exec } = require("child_process");
+const { spawn } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
 const dirString = path.dirname(fs.realpathSync(__filename));
 
-exec("./node_modules/aws-cdk/bin/cdk deploy --require-approval never", {cwd: dirString}, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+process.env['@greyshipscode/aws-node-pipeline/stack-name'] = 'FakeName';
+
+const runDeploy = spawn(dirString + '/node_modules/aws-cdk/bin/cdk', ['deploy', '--require-approval', 'never'], {
+    cwd: dirString
+});
+
+runDeploy.stdout.on('data', (data) => {
+  console.info(`aws-node-pipeline: ${data}`);
+});
+
+runDeploy.stderr.on('data', (data) => {
+  console.error(`[DEPLOYMENT]: ${data}`);
+});
+
+runDeploy.on('error', (error) => {
+  console.error("Unable to spawn child process.");
+});
+
+runDeploy.on('close', (code) => {
+  switch(code) {
+    case 1:
+      return console.error("[ERROR] aws-node-pipline: AWS CDK experienced an error during deployment.");
+    case 0:
+      return console.info("aws-node-pipeline: Deployment completed successfully.");
+    default:
+      return console.error("[ERROR] aws-node-pipeline: Child process exited with status code " + code);
+  }
 });
